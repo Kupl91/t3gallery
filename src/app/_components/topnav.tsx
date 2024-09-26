60e2b4067cf012110bfc8383e7cd7cb8fac050fd
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-09-26 23:46:16 +0300

    images almost uploding

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,7 +1,13 @@
+'use client';
+
 import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav() {
+    const router = useRouter()
+
+
     return (
       <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
         <div>Gallery</div>
@@ -11,7 +17,11 @@ export function TopNav() {
                 <SignInButton />
             </SignedOut>
             <SignedIn>
-                <UploadButton endpoint="imageUploader"  />
+                <UploadButton endpoint="imageUploader" 
+                onClientUploadComplete={() => {
+                    router.refresh();
+                }}
+                />
                 <UserButton />
             </SignedIn>
         </div>
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,32 +1,32 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+import { db } from "~/server/db";
+import { images } from "~/server/db/schema";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+const auth = (req: Request) => ({ id: "fakeId", userId: "yourUserID" }); // Реализуйте соответствующую аутентификацию
  
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
       const user = await auth(req);
 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
+      if (!user.userId) throw new UploadThingError("Unauthorized");
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
+      return { userId: user.userId}; // Возвращаем значение как metadata
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
+      console.log("Upload complete for user:", metadata.userId);
 
-      console.log("file url", file.url);
+      await db.insert(images).values({
+        userId: metadata.userId,
+        name: file.name,
+        url: file.url,
+      });
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      console.log("file url:", file.url);
+
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
