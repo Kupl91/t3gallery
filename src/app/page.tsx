af60ed5b9007dfdc725e517ced1d9664cdb19879
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-09-26 13:50:27 +0300

    shmel photo added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Link from "next/link";
 
+
+const mockUrls = [
+  "https://utfs.io/f/bc9aLWGXt2mZLbcgRg3HK5c2bJ93Ty6VAEFNCh1SwPzxu4on",
+  "https://utfs.io/f/bc9aLWGXt2mZ7npOErSanNpqQU8LBK45ofsVMyWTb3rXSRZH"
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-       hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap">
+        {mockImages.map((image) => (
+          <div key={image.id} className='w-1/2 p-4'>
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+        hello (gallery in progress)
       </div>
     </main>
   );
