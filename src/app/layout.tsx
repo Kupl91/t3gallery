e3faf1c19fcf0001993a6a2aafd4fa3090e65871
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-09-26 14:32:09 +0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,25 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+    </nav>
+  );
+}
+
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${GeistSans.variable}flex-col gap-4`}>
+      <body className={'font-sans ${inter.variable}'}>
+        <TopNav />
+        {children}
+        </body>
     </html>
   );
 }
